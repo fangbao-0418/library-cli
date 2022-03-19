6224fc610fda1cd790f3575896949db0217ef14f
junyi <junyi.fang@tuya.com>
2022-03-19 15:17:06 +0800

    feat: babel

--- target/compoments/button/index.tsx
+++ target/compoments/button/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import 'core-js'
+import React, { useEffect } from 'react'
 
 const Button = () => {
+
+  useEffect(() => {
+    const t = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(2)
+      }, 1000)
+    })
+  }, [])
+
   return (
     <button>button</button>
   )
